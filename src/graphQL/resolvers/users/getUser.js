fb416776334b5722de3bfc51e755fcfbfde5ce50
Jalen Jackson <jalenjack201@hotmail.com>
2019-03-11 17:15:49 -0400

    remove hardcoded user string

--- src/graphQL/resolvers/users/getUser.js
+++ src/graphQL/resolvers/users/getUser.js
@@ -4,7 +4,7 @@ const { TransformObject } = require('./merge');
 
 exports.getUser = async (args, req) => {
   try {
-    const user = await User.findById('5c7089f44b5d515c8caad3de');
+    const user = await User.findById(req.userId);
     return TransformObject(user);
   } catch (e) {
     throw e;
