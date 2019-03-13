152cd0136814d03522f31741d5af601cf382368f
Jalen Jackson <jalenjack201@hotmail.com>
2019-03-13 02:15:28 -0400

    permission changes

--- src/index.js
+++ src/index.js
@@ -30,8 +30,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, POST, PATCH, GET');
+  
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json({});
   }
   next();
@@ -52,6 +53,4 @@ app.use(function(req, res) {
   })
 });
 
-const server = http.createServer(app);
-
-server.listen(PORT);
+app.listen(PORT, () => console.log('---------- Course Camp API Running --------'));
