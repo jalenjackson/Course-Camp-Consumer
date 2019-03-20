f8fda8db7f32c8a31c953d8f99cc44237315a29b
Jalen Jackson <jalenjack201@hotmail.com>
2019-03-20 11:29:05 -0400

    remove env

---
+++ src/APIRoutes/getVideoUrl.js
@@ -0,0 +1,3 @@
+exports.call = (req, res) => {
+
+};
--- src/APIRoutes/index.js
+++ src/APIRoutes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const VerifyAuthenticationStrict = require('../middleware/verifyAuthenticationStrict');
 const reorderSections = require('./sections/reorderSections');
 const changeCourseStatus = require('./changeCourseStatus');
+const getVideoUrl = require('./getVideoUrl');
 const router = express.Router();
 
 router.post('/reorder-sections', VerifyAuthenticationStrict.verify, reorderSections.call);
 router.post('/change-course-status', changeCourseStatus.call);
+router.get('/get-video-url', getVideoUrl.call);
 
 module.exports = router;
